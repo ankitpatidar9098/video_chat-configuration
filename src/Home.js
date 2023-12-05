881737a1ceb6560d30e2196946af94fa61851032
Ankit Patidar <ankitpatidar01650@gmail.com>
2023-12-05 15:40:17 +0530

    first config setup

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css';
+
+function Home() {
+  
+  return (
+    <div className='App'> 
+      <input placeholder='Enter Rome ID' type='text'></input>
+      <button> Join bruh</button>
+    </div>
+  )
+}
+
+export default Home
